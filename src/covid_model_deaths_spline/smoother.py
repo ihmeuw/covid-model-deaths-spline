57daa9c014b706dab2cf6f0de4cc763e88b96fd7
Ryan Barber <rmbarber@uw.edu>
2020-05-31 13:39:46 -0700

    fix plot for deaths only

--- src/covid_model_deaths_spline/smoother.py
+++ src/covid_model_deaths_spline/smoother.py
@@ -183,6 +183,16 @@ def synthesize_time_series(location_id: int,
     return draw_df
 
 
+def get_plot_idx(i: int, n_vars: int):
+    if len(unadj_vars) > 1:
+        top_idx = 0, i
+        bottom_idx = 1, i
+    else:
+        top_idx = 0
+        bottom_idx = 1
+    return top_idx, bottom_idx
+
+
 def plotter(df: pd.DataFrame, unadj_vars: List[str], plot_file: str):
     # set up plot
     sns.set_style('whitegrid')
@@ -226,57 +236,61 @@ def plotter(df: pd.DataFrame, unadj_vars: List[str], plot_file: str):
         ax[0, indep_idx].set_xlabel('Cumulative hospitalization rate', fontsize=10)
         ax[0, indep_idx].set_ylabel('Cumulative death rate', fontsize=10)
 
+        
     for i, smooth_variable in enumerate(unadj_vars):
+        top_idx, bottom_idx = get_plot_idx(i, len(unadj_vars))
+        
         # cumulative
         raw_variable = smooth_variable.replace('Smoothed ', '').capitalize()
         plot_label = raw_variable.lower().replace(' rate', 's')
         if ~df[raw_variable].isnull().all():
             if 'death' in smooth_variable.lower():
-                ax[0, i].plot(df['Date'], df[raw_variable] * df['population'], **raw_lines)
-                ax[0, i].scatter(df['Date'], df[raw_variable] * df['population'], **raw_points)
-                ax[1, i].set_xlabel('Date', fontsize=10)
-                ax[0, i].set_ylabel(f'Cumulative {plot_label}', fontsize=10)
+                ax[top_idx].plot(df['Date'], df[raw_variable] * df['population'], **raw_lines)
+                ax[top_idx].scatter(df['Date'], df[raw_variable] * df['population'], **raw_points)
+                ax[bottom_idx].set_xlabel('Date', fontsize=10)
+                ax[top_idx].set_ylabel(f'Cumulative {plot_label}', fontsize=10)
 
             # daily
-            ax[1, i].plot(df['Date'][1:],
-                          np.diff(df[raw_variable]) * df['population'][1:],
-                          **raw_lines)
-            ax[1, i].scatter(df['Date'][1:],
-                             np.diff(df[raw_variable]) * df['population'][1:],
-                             **raw_points)
-            ax[1, i].axhline(0, color='black', alpha=0.25)
+            ax[bottom_idx].plot(df['Date'][1:],
+                                np.diff(df[raw_variable]) * df['population'][1:],
+                                **raw_lines)
+            ax[bottom_idx].scatter(df['Date'][1:],
+                                   np.diff(df[raw_variable]) * df['population'][1:],
+                                   **raw_points)
+            ax[bottom_idx].axhline(0, color='black', alpha=0.25)
             if 'death' in smooth_variable.lower():
-                ax[1, i].set_xlabel('Date', fontsize=10)
+                ax[bottom_idx].set_xlabel('Date', fontsize=10)
             else:
-                ax[1, i].set_xlabel('Date (+8 days)', fontsize=10)
-            ax[1, i].set_ylabel(f'Daily {plot_label}', fontsize=10)
+                ax[bottom_idx].set_xlabel('Date (+8 days)', fontsize=10)
+            ax[bottom_idx].set_ylabel(f'Daily {plot_label}', fontsize=10)
 
     # model prediction
-    ax[0, 0].plot(df['Date'], df['Predicted death rate (CFR)'] * df['population'],
-                  **cfr_lines)
-    ax[0, 0].plot(df['Date'], df['Predicted death rate (HFR)'] * df['population'],
-                  **hfr_lines)
-    ax[1, 0].plot(df['Date'][1:],
-                  np.diff(df['Predicted death rate (CFR)']) * df['population'][1:],
-                  **cfr_lines)
-    ax[1, 0].plot(df['Date'][1:],
-                  np.diff(df['Predicted death rate (HFR)']) * df['population'][1:],
-                  **hfr_lines)
+    top_idx, bottom_idx = get_plot_idx(0, len(unadj_vars))
+    ax[top_idx].plot(df['Date'], df['Predicted death rate (CFR)'] * df['population'],
+                     **cfr_lines)
+    ax[top_idx].plot(df['Date'], df['Predicted death rate (HFR)'] * df['population'],
+                     **hfr_lines)
+    ax[bottom_idx].plot(df['Date'][1:],
+                        np.diff(df['Predicted death rate (CFR)']) * df['population'][1:],
+                        **cfr_lines)
+    ax[bottom_idx].plot(df['Date'][1:],
+                        np.diff(df['Predicted death rate (HFR)']) * df['population'][1:],
+                        **hfr_lines)
 
     # smoothed
-    ax[0, 0].plot(df['Date'],
-                  df['Smoothed predicted death rate'] * df['population'],
-                  **smoothed_pred_lines)
-    ax[0, 0].fill_between(
+    ax[top_idx].plot(df['Date'],
+                     df['Smoothed predicted death rate'] * df['population'],
+                     **smoothed_pred_lines)
+    ax[top_idx].fill_between(
         df['Date'],
         df['Smoothed predicted death rate lower'] * df['population'],
         df['Smoothed predicted death rate upper'] * df['population'],
         **smoothed_pred_area
     )
-    ax[1, 0].plot(df['Date'],
-                  df['Smoothed predicted daily death rate'] * df['population'],
-                  **smoothed_pred_lines)
-    ax[1, 0].fill_between(
+    ax[bottom_idx].plot(df['Date'],
+                        df['Smoothed predicted daily death rate'] * df['population'],
+                        **smoothed_pred_lines)
+    ax[bottom_idx].fill_between(
         df['Date'],
         df['Smoothed predicted daily death rate lower'] * df['population'],
         df['Smoothed predicted daily death rate upper'] * df['population'],

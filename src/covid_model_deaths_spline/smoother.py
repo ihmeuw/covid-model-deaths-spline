31a5a1c20fc457b94d6a818d3c13116d4b429706
Ryan Barber <rmbarber@uw.edu>
2020-06-03 15:10:31 -0700

    add priors on slope

--- src/covid_model_deaths_spline/smoother.py
+++ src/covid_model_deaths_spline/smoother.py
@@ -30,7 +30,7 @@ def run_smoothing_model(mod_df: pd.DataFrame, n_i_knots: int, spline_options: Di
             ensemble_knots=ensemble_knots,
             scale_se=scale_se,
             observed_var='observed',
-            pseudo_se_multiplier=1.33
+            pseudo_se_multiplier=1.25
         )
         mr_model.fit_model()
         smooth_y = mr_model.predict(pred_df)
@@ -41,7 +41,8 @@ def run_smoothing_model(mod_df: pd.DataFrame, n_i_knots: int, spline_options: Di
             return smooth_y
         
 
-def process_inputs(y: np.array, x: np.array, subset_idx: np.array, mono: bool):
+def process_inputs(y: np.array, x: np.array, n_i_knots: int, subset_idx: np.array, 
+                   mono: bool, tail_gprior: np.array = None):
     # get smoothed curve (dropping NAs, inflating variance for pseudo-deaths)
     obs_data = y[subset_idx].copy()
     obs_data[:,0] = 1
@@ -68,6 +69,12 @@ def process_inputs(y: np.array, x: np.array, subset_idx: np.array, mono: bool):
         }
     if mono:
         spline_options.update({'prior_spline_monotonicity': 'increasing'})
+    if tail_gprior is not None:
+        maxder_gprior = np.array([[0, np.inf]] * (n_i_knots + 1)).T
+        if tail_gprior.size != 2:
+            raise ValueError('`tail_gprior` must be in the format np.array([mu, sigma])')
+        maxder_gprior[:,-1] = tail_gprior
+        spline_options.update({'prior_spline_maxder_gaussian': maxder_gprior})
     
     return mod_df, spline_options
     
@@ -99,30 +106,41 @@ def smoother(df: pd.DataFrame, obs_var: str, pred_vars: List[str],
     ln_cumul_y = np.log(apply_floor(cumul_y, floor))
     ln_daily_y = np.log(apply_floor(daily_y, floor))
     x = df.index[keep_idx].values
+    
+    # get deaths in last week
+    last_week = df.copy()
+    last_week['Deaths'] = last_week['Death rate'] * last_week['population']
+    last_week['Deaths'][1:] = np.diff(last_week['Deaths'])
+    last_week_deaths = last_week.loc[~last_week['Deaths'].isnull()].iloc[-7:]['Deaths'].sum()
+    gprior_se = (last_week_deaths + 0.1) * 10
 
-    # prepare data
-    ln_cumul_mod_df, ln_cumul_spline_options = process_inputs(
-        y=ln_cumul_y, x=x, subset_idx=max_1week_of_zeros, mono=True
-    )
-    ln_daily_mod_df, ln_daily_spline_options = process_inputs(
-        y=ln_daily_y, x=x, subset_idx=max_1week_of_zeros, mono=False
-    )
+    # prepare data and run daily
     pred_df = pd.DataFrame({'intercept':1, 'x': x})
-    ln_cumul_smooth_y, ensemble_knots = run_smoothing_model(
-        ln_cumul_mod_df, n_i_knots, ln_cumul_spline_options, False, pred_df
+    ln_daily_mod_df, ln_daily_spline_options = process_inputs(
+        y=ln_daily_y, x=x, n_i_knots=n_i_knots, subset_idx=max_1week_of_zeros, 
+        mono=False, tail_gprior=np.array([0, gprior_se])
     )
-    ln_daily_smooth_y = run_smoothing_model(
-        ln_daily_mod_df, n_i_knots, ln_daily_spline_options, True, pred_df, ensemble_knots
+    ln_daily_smooth_y, ensemble_knots = run_smoothing_model(
+        ln_daily_mod_df, n_i_knots, ln_daily_spline_options, True, pred_df
     )
     
-    # average the two in linear daily, then log
+    # run cumulative, using slope after the last knot as the prior mean
+    cutoff = x.min() + ensemble_knots[0][-2] * x.ptp()
+    slope = ln_daily_smooth_y[x > cutoff].mean()
+    ln_cumul_mod_df, ln_cumul_spline_options = process_inputs(
+        y=ln_cumul_y, x=x, n_i_knots=n_i_knots, subset_idx=max_1week_of_zeros, 
+        mono=True, tail_gprior=np.array([slope, gprior_se])
+    )
+    ln_cumul_smooth_y = run_smoothing_model(
+        ln_cumul_mod_df, n_i_knots, ln_cumul_spline_options, False, pred_df, ensemble_knots
+    )
+    
+    # average the two in linear daily (increasing influence of daily as we get closer to 100), then log
     from_cumul = np.exp(ln_cumul_smooth_y)
     from_cumul[1:] = np.diff(from_cumul)
     from_daily = np.exp(ln_daily_smooth_y)
-    d_w = min(max(0, total_deaths - 50.) / 50., 
-              1.)
-    c_w = min(max(0, 100. - total_deaths) / 50.,
-              1.)
+    d_w = min(total_deaths / 100., 1.)
+    c_w = 1. - d_w
     smooth_y = from_daily * d_w + from_cumul * c_w
     smooth_y = np.log(smooth_y)
 

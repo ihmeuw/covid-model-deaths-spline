112fc24aa50316c54f4f534fd7d2df96dd90ab56
James Collins <collijk@uw.edu>
2020-06-04 15:57:17 -0700

    log data sins

--- src/covid_model_deaths_spline/data.py
+++ src/covid_model_deaths_spline/data.py
@@ -1,22 +1,27 @@
 import functools
 from pathlib import Path
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 from loguru import logger
 import pandas as pd
 import numpy as np
 
 
-def evil_doings(case_data: pd.DataFrame, hosp_data: pd.DataFrame, death_data: pd.DataFrame) -> Tuple[pd.DataFrame]:
+def evil_doings(case_data: pd.DataFrame,
+                hosp_data: pd.DataFrame,
+                death_data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, Dict[str, str]]:
+    # Record our sins
+    manipulation_metadata = {}
     # even out days in Tennessee with spike in reporting
     case_data = case_data.copy()
     tn_df = case_data.loc[case_data['location_id'] == 565]
-    bad_days = (tn_df['True date'] >= pd.to_datetime('2020-05-31')) & (tn_df['True date'] <= pd.to_datetime('2020-06-02'))
+    bad_days = ((tn_df['True date'] >= pd.to_datetime('2020-05-31'))
+                & (tn_df['True date'] <= pd.to_datetime('2020-06-02')))
     tn_df.loc[bad_days, 'Confirmed case rate'] = np.nan
     new_tn = tn_df['Confirmed case rate'].interpolate().values
     case_data.loc[case_data['location_id'] == 565, 'Confirmed case rate'] = new_tn
-    
-    return case_data, hosp_data, death_data
+    manipulation_metadata['tennessee'] = 'even out cases spike between 5/31 and 6/2'
+    return case_data, hosp_data, death_data, manipulation_metadata
 
 
 def load_most_detailed_locations(inputs_root: Path) -> pd.DataFrame:
@@ -156,15 +161,15 @@ def check_counts(model_data: pd.DataFrame, rate_var: str, action: str, threshold
     return df
 
 
-def filter_to_epi_threshold(hierarchy: pd.DataFrame, 
-                            model_data: pd.DataFrame, 
-                            threshold: int = 3) -> Tuple[pd.DataFrame, List[int]]:
+def filter_to_epi_threshold(hierarchy: pd.DataFrame,
+                            model_data: pd.DataFrame,
+                            threshold: int = 3) -> Tuple[pd.DataFrame, List[int], List[int]]:
     """Drop locations that don't have at least `n` deaths; do not use cases or hospitalizations if under `n`."""
     df = model_data.copy()
     df = check_counts(df, 'Confirmed case rate', 'fill_na', threshold)
     days_w_cases = df['Confirmed case rate'].notnull().groupby(df['location_id']).sum()
     no_cases_locs = days_w_cases[days_w_cases == 0].index.to_list()
-    
+
     df = check_counts(df, 'Hospitalization rate', 'fill_na', threshold)
     days_w_hosp = df['Hospitalization rate'].notnull().groupby(df['location_id']).sum()
     no_hosp_locs = days_w_hosp[days_w_hosp == 0].index.to_list()
@@ -185,5 +190,5 @@ def fill_dates(df: pd.DataFrame, interp_var: str = None) -> pd.DataFrame:
     if interp_var:
         df[interp_var] = df[interp_var].interpolate()
     df = df.fillna(method='pad')
-    
+
     return df
--- src/covid_model_deaths_spline/runner.py
+++ src/covid_model_deaths_spline/runner.py
@@ -14,14 +14,12 @@ warnings.simplefilter('ignore')
 
 def make_deaths(app_metadata: cli_tools.Metadata, input_root: Path, output_root: Path,
                 holdout_days: int):
-    #
     logger.debug("Setting up output directories.")
     model_dir = output_root / 'models'
     plot_dir = output_root / 'plots'
     shell_tools.mkdir(model_dir)
     shell_tools.mkdir(plot_dir)
 
-    #
     logger.debug("Loading and cleaning data.")
     hierarchy = data.load_most_detailed_locations(input_root)
     full_data = data.load_full_data(input_root)
@@ -29,18 +27,16 @@ def make_deaths(app_metadata: cli_tools.Metadata, input_root: Path, output_root:
     hosp_data = data.get_shifted_data(full_data, 'Hospitalizations', 'Hospitalization rate')
     death_data = data.get_death_data(full_data)
     pop_data = data.get_population_data(full_data)
-    
-    #
-    logger.debug("Data manipulation")
-    case_data, hosp_data, death_data = data.evil_doings(case_data, hosp_data, death_data)
 
-    #
+    logger.debug("Data manipulation")
+    case_data, hosp_data, death_data, manipulation_metadata = data.evil_doings(case_data, hosp_data, death_data)
+    app_metadata.update({'data_manipulation': manipulation_metadata})
+
     logger.debug(f"Dropping {holdout_days} days from the end of the data.")
     case_data = data.holdout_days(case_data, holdout_days)
     hosp_data = data.holdout_days(hosp_data, holdout_days)
     death_data = data.holdout_days(death_data, holdout_days)
 
-    #
     logger.debug(f"Filtering data by location.")
     case_data, missing_cases = data.filter_data_by_location(case_data, hierarchy, 'cases')
     hosp_data, missing_hosp = data.filter_data_by_location(hosp_data, hierarchy, 'hospitalizations')
@@ -49,7 +45,6 @@ def make_deaths(app_metadata: cli_tools.Metadata, input_root: Path, output_root:
     model_data = data.combine_data(case_data, hosp_data, death_data, pop_data, hierarchy)
     model_data, no_cases_locs, no_hosp_locs = data.filter_to_epi_threshold(hierarchy, model_data, 1)
 
-    #
     logger.debug('Preparing model settings.')
     model_settings = {}
     s1_settings = {'dep_var': 'Death rate',
@@ -73,7 +68,6 @@ def make_deaths(app_metadata: cli_tools.Metadata, input_root: Path, output_root:
     model_settings['no_cases_locs'] = no_cases_locs
     model_settings['no_hosp_locs'] = no_hosp_locs
 
-    #
     logger.debug('Launching models by location.')
     working_dir = output_root / 'model_working_dir'
     shell_tools.mkdir(working_dir)
@@ -91,8 +85,7 @@ def make_deaths(app_metadata: cli_tools.Metadata, input_root: Path, output_root:
         for location_id in model_data['location_id'].unique()
     }
     cluster.run_cluster_jobs('covid_death_models', output_root, job_args_map)
-    
-    #
+
     logger.debug('Compiling results.')
     results = []
     for result_path in results_path.iterdir():
@@ -106,7 +99,6 @@ def make_deaths(app_metadata: cli_tools.Metadata, input_root: Path, output_root:
     logger.debug("Synthesizing plots.")
     pdf_merger.pdf_merger(indir=plot_dir, outfile=str(output_root / 'model_results.pdf'))
 
-    #
     logger.debug("Writing output data.")
     model_data = model_data.rename(columns={'Date': 'date'}).set_index(['location_id', 'date'])
     noisy_draws = noisy_draws.set_index(['location_id', 'date'])
@@ -116,4 +108,3 @@ def make_deaths(app_metadata: cli_tools.Metadata, input_root: Path, output_root:
     model_data.rename(columns={'date': 'Date'}).reset_index().to_csv(output_root / 'model_data.csv', index=False)
     noisy_draws.reset_index().to_csv(output_root / 'model_results.csv', index=False)
     smooth_draws.reset_index().to_csv(output_root / 'model_results_refit.csv', index=False)
-    

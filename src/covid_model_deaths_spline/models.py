9186a6ccc95a21a95b2f7c895d3c0cd1f1e782a9
Ryan Barber <rmbarber@uw.edu>
2020-06-24 13:54:18 -0700

    whitespace

--- src/covid_model_deaths_spline/models.py
+++ src/covid_model_deaths_spline/models.py
@@ -38,15 +38,15 @@ def model_iteration(location_id: int, model_data: pd.DataFrame, model_settings: 
     model_data = model_data.loc[~model_data[indicators].isnull().all(axis=1)]
     
     # first stage model(s)
-    model_data_list = [model_data.loc[:,['location_id', 'location_name', 'Date', 
+    model_data_list = [model_data.loc[:,['location_id', 'location_name', 'Date',
                                          'Death rate', 'population']]]
     if location_id not in model_settings['no_cases_locs']:
         cfr_model_data = cfr_model.cfr_model(model_data, **model_settings['CFR'])
-        model_data_list += [cfr_model_data.loc[:, ['location_id', 'Date', 
+        model_data_list += [cfr_model_data.loc[:, ['location_id', 'Date',
                                                    'Confirmed case rate', 'Predicted death rate (CFR)']]]
     if location_id not in model_settings['no_hosp_locs']:
         hfr_model_data = cfr_model.cfr_model(model_data, **model_settings['HFR'])
-        model_data_list += [hfr_model_data.loc[:, ['location_id', 'Date', 
+        model_data_list += [hfr_model_data.loc[:, ['location_id', 'Date',
                                                    'Hospitalization rate', 'Predicted death rate (HFR)']]]
     
     # combine outputs

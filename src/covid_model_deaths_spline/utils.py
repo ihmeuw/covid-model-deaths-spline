a1648664950d7a1a2b4ead2b7deadfbb362a6e06
Ryan Barber <rmbarber@uw.edu>
2020-07-07 16:41:17 -0700

    store away nrmse for later

--- src/covid_model_deaths_spline/utils.py
+++ src/covid_model_deaths_spline/utils.py
@@ -10,3 +10,24 @@ def get_data_se(data: np.array, log: bool = True) -> np.array:
     
     return se
     
+
+## ???
+# def get_pseudo_nrmse(data: pd.DataFrame, 
+#                      obs_var: str, pred_vars: List[str], 
+#                      interval: int = 7) -> float:
+#     # only days with both
+#     no_na = data[[obs_var, pred_var]].notnull().all(axis=1)
+#     data = data.loc[no_na, [obs_var, pred_var]]
+    
+#     # convert to daily
+#     data = np.diff(data, axis=0, prepend=0)
+    
+#     # keep specified days
+#     data = data[-interval:,:]
+    
+#     # get mean, then get rmse
+#     data_range = data[:,0].ptp()
+#     rmse = np.sqrt(np.mean(np.diff(data, axis=1)**2))
+#     nrmse = rmse / data_range
+    
+#     return nrmse
